39f00cbcc047c94adbf1a6e8cb73c06f4e46c88b
Karthika Jayaprakash <karthikajayaprakash@Karthikas-MacBook-Air.local>
2025-04-09 23:19:23 -0700

    Rename

--- generate-keypair/generate-keypair-from-env.ts
+++
@@ -1,8 +0,0 @@
-import { getKeypairFromEnvironment } from "@solana-developers/helpers";
-import "dotenv/config";
-
-const keypair = getKeypairFromEnvironment("SECRET_KEY");
-console.log('Generated KeyPair');
-console.log('Public Key : ', keypair.publicKey);
-console.log('Loaded Secret Key : ', keypair.secretKey);
-console.log('Finished');
